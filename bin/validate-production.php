2ca85980ffa350075bde10d7863a168188826b18
MegaMan-Ofc <tiagoc8ribeiro@gmail.com>
2026-08-06 10:04:55 +0100

    chore(deploy): add production validation safeguards

---
+++ bin/validate-production.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+if (PHP_SAPI !== 'cli') {
+    http_response_code(404);
+    exit;
+}
+
+require_once dirname(__DIR__) . '/includes/config.php';
+
+$errors = configuration_errors();
+
+if ($errors !== []) {
+    foreach ($errors as $error) {
+        fwrite(STDERR, '- ' . $error . PHP_EOL);
+    }
+
+    exit(1);
+}
+
+fwrite(STDOUT, 'Production configuration is valid.' . PHP_EOL);
+exit(0);
